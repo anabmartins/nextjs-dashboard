bc30d4bf4598bff257fe927a8344ff5a2ac96936
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-12-08 09:20:39 -0300

    :rocket: optimize navigation

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,4 +9,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
-}
+}   
